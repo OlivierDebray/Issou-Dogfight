b8c9c995e4ce0333b83e3b1574f0c8a32265c502
OlivierDebray <olivier.debray@viacesi.fr>
2017-06-01 10:24:30 +0200

    Ajout IOrderPerformer

---
+++ src/issou_dogfight/dogfight/controller/IOrderPerformer.java
@@ -0,0 +1,9 @@
+package issou_dogfight.dogfight.controller;
+
+/**
+ * Created by Olivier Debray on 01/06/2017.
+ */
+
+public interface IOrderPerformer {
+    void orderPerform (UserOrder userOrder) ;
+}
