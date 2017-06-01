c34575936d99b2c0bd8bfbc4b75218a9f6a2fdb0
sambao407 <sambao407@gmail.com>
2017-06-01 11:51:12 +0200

    Add files via upload

---
+++ src/issou_dogfight/dogfight/view/DogFightView.java
@@ -0,0 +1,21 @@
+package issou_dogfight.dogfight.view;
+
+import java.util.Observable;
+
+public class DogFightView {
+	public DogfightView(IOrderPerformer orderPerformer, IDogfightModel dogfightModel, Observable observable) 
+	
+	public void run() {
+		
+	}
+	
+	public void displayMessage(String message) {
+		
+	}
+	
+	
+	public void closeAll() {
+		
+	}
+
+}
