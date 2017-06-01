91bf097e9d685f046b5fc3a26f2a8e94aac1f6bb
sambao407 <sambao407@gmail.com>
2017-06-01 11:47:09 +0200

    Add files via upload

---
+++ src/issou_dogfight/dogfight/EventPerformer.java
@@ -0,0 +1,20 @@
+package issou_dogfight.dogfight.view;
+
+import com.sun.glass.events.KeyEvent;
+
+public class EventPerformer {
+	public EventPerformer(IOrderPerformer orderPerformer) {
+		
+	}
+	
+	
+	public void eventPerform(KeyEvent KeyCode) {
+		
+	}
+	
+	public  keyCodeToUserOrder keyCodeToUserOrder(int keyCode) {
+		
+	}
+	
+
+}
