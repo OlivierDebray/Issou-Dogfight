913a2270dad6f70485c4de222a0ca5e82f1db499
OlivierDebray <olivier.debray@viacesi.fr>
2017-06-02 09:28:46 +0200

    Update final

--- src/Static.java
+++ src/Static.java
@@ -10,8 +10,7 @@ public class Static {
     public static void main (String[] args) {
         final DogfightModel dogfightModel = new DogfightModel();
         final DogfightController dogfightController = new DogfightController(dogfightModel);
-        final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel,
-                dogfightModel);
+        final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel, dogfightModel);
         dogfightController.setViewSystem(dogfightView);
         dogfightController.play();
     }
