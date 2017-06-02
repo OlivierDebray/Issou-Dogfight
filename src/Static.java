96c43d27808648d91b39f8702ca6757bffcbd472
OlivierDebray <olivier.debray@viacesi.fr>
2017-06-02 09:23:49 +0200

    Ajout main

--- src/Static.java
+++ src/Static.java
@@ -1,9 +1,18 @@
+import issou_dogfight.dogfight.controller.DogfightController;
+import issou_dogfight.dogfight.model.DogfightModel;
+import issou_dogfight.dogfight.view.DogfightView;
+
 /**
  * Created by Olivier Debray on 01/06/2017.
  */
 
 public class Static {
     public void main (String[] args) {
-
+        final DogfightModel dogfightModel = new DogfightModel();
+        final DogfightController dogfightController = new DogfightController(dogfightModel);
+        final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel,
+                dogfightModel);
+        dogfightController.setViewSystem(dogfightView);
+        dogfightController.play();
     }
 }
