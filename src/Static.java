7db517e6c3f27052a1d049bd808ff681f6b02d94
OlivierDebray <olivier.debray@viacesi.fr>
2017-06-02 09:25:34 +0200

    Update main

--- src/Static.java
+++ src/Static.java
@@ -7,7 +7,7 @@ import issou_dogfight.dogfight.view.DogfightView;
  */
 
 public class Static {
-    public void main (String[] args) {
+    public static void main (String[] args) {
         final DogfightModel dogfightModel = new DogfightModel();
         final DogfightController dogfightController = new DogfightController(dogfightModel);
         final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel,
